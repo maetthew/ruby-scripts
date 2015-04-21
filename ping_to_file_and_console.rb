98d2c64873a5c009c6bfb0d0f923a7e9accec1c7
maetthew <hello@mattiasagar.com>
2015-04-21 02:19:23 +0200

    Comment update, added domain variable

--- ping_to_file_and_console.rb
+++ ping_to_file_and_console.rb
@@ -1,12 +1,13 @@
-# Basically the pinging and writing to file is done by the shell command in POpen4::popen4 and the console output is handled by Ruby
+# Pings a host and writes it to a .txt file named as a timestamp
 
 require 'rubygems'
 require 'popen4'
 
 def run
 	time = Time.new.to_i.to_s + ".txt"
+	domain = google.com
 
-    POpen4::popen4("ping -i2 google.com | while read pong; do echo \"$(date +\'%c\') | $pong\"; done | tee #{time}") do |stdout, stderr, stdin, pid|
+    POpen4::popen4("ping -i2 #{domain} | while read pong; do echo \"$(date +\'%c\') | $pong\"; done | tee #{time}") do |stdout, stderr, stdin, pid|
     	while s = stdout.gets
         	puts s
         end
